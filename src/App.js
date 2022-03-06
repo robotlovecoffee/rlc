2eb5783b6a09d59e552f75db61e4d919607118c3
robotlovecoffee <96776239+robotlovecoffee@users.noreply.github.com>
2022-03-06 12:54:18 -0500

    live

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import fleekLogo from './fleek-logo.png';
-import reactLogo from './react-logo.svg';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <span className="Big-plus">+</span>
-        <p>
-Robot Love Coffee
-        </p>
-        <a
-          className="App-link"
-          href="https://docs.fleek.co"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-       Test
-        </a>
-      </header>
     </div>
   );
 }
